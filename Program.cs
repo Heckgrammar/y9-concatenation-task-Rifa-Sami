0522b9017cb03aba09783dec556646ee541bd5db
Rifa <23RSami@heckgrammar.co.uk>
2026-02-02 09:52:43 +0000

    Concatenation Task
    Added functionality to collect user's last name and age, and output a greeting message.

--- Program.cs
+++ Program.cs
@@ -4,17 +4,29 @@
     {
         static void Main(string[] args)
         {
-            string FirstName;
-            Console.Write("Enter first name: ");
-            FirstName = Console.ReadLine();
+            
             // add two more variables for the users last name and their age
             // take these inputs into the program
             // use the test data Bob, Smith, 14
             // ammend this code to output in one command
             // Hello Bob Smith you are 14 years old
-            Console.WriteLine(FirstName);
             // save and commit your program
             // add evidence of the test input and your own input running the program 
+
+            
+            string FirstName;
+            Console.Write("Enter first name: ");
+            FirstName = Console.ReadLine();
+
+            string LastName;
+            Console.Write("Enter your last name: ");
+            LastName = Console.ReadLine();
+
+            string Age;
+            Console.Write("Enter your age: ");
+            Age = Console.ReadLine();
+
+            Console.WriteLine("Hello " + FirstName + " " + LastName + " you are " + Age + " years old");
         }
     }
 }
